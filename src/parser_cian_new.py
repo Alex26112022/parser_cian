a05fb5ab2a73371d0c79de867b2faeccca2bfb04
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-05 21:12:07 +0300

    new parser

--- src/parser_cian_new.py
+++ src/parser_cian_new.py
@@ -2,11 +2,17 @@ import requests
 from bs4 import BeautifulSoup
 from fake_headers import Headers
 
-url = 'https://www.cian.ru/kupit-kvartiru-novostroyki/'
+full_url = 'https://www.cian.ru/cat.php?currency=2&deal_type=sale&decorations_list%5B0%5D=without&engine_version=2&house_material%5B0%5D=2&maxfloor=10&maxprice=15000000&maxtarea=100&minfloor=2&minprice=5000000&mintarea=60&object_type%5B0%5D=2&offer_type=flat&only_flat=1&region=4593&room3=1&year%5B0%5D=2026'
+url = 'https://www.cian.ru/cat.php'
 headers = Headers(os="win", headers=True).generate()
+params = {'currency': '2', 'deal_type': 'sale',
+          'decorations_list%5B0%5D': 'without', 'engine_version': '2',
+          'house_material%5B0%5D': '2', 'maxfloor': '10',
+          'maxprice': '15000000', 'maxtarea': '100', 'minfloor': '2',
+          'minprice': '6000000', 'mintarea': '60', 'object_type%5B0%5D': '2',
+          'offer_type': 'flat', 'only_flat': '1', 'region': '4593',
+          'room3': '1', 'year%5B0%5D': '2026'}
 
-response = requests.get(url, headers=headers)
+response = requests.get(url, headers=headers, params=params)
 
 soup = BeautifulSoup(response.text, 'lxml')
-
-print(soup)
