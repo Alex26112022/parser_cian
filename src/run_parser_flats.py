7e3f413e29d88cd4143a4d79c855aaf48e6ddc3a
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-06 11:12:18 +0300

    refactoring

--- src/run_parser_flats.py
+++ src/run_parser_flats.py
@@ -9,21 +9,21 @@ from src.parser_cian import ParserCian
 
 def run_parser():
     """ Запускает главный цикл парсера готового жилья. """
-    # input_data = interactive_func()
-    # print(input_data)
-    #
-    # new_search = ParserCian(input_data[0])
-    # flats = new_search.get_search_flats(rooms=input_data[1],
-    #                                     object_type=input_data[2],
-    #                                     min_price=input_data[3],
-    #                                     max_price=input_data[4],
-    #                                     min_floor=input_data[5],
-    #                                     max_floor=input_data[6],
-    #                                     min_total_floor=input_data[7],
-    #                                     max_total_floor=input_data[8])
+    input_data = interactive_func()
+    print(input_data)
+
+    new_search = ParserCian(input_data[0])
+    flats = new_search.get_search_flats(rooms=input_data[1],
+                                        object_type=input_data[2],
+                                        min_price=input_data[3],
+                                        max_price=input_data[4],
+                                        min_floor=input_data[5],
+                                        max_floor=input_data[6],
+                                        min_total_floor=input_data[7],
+                                        max_total_floor=input_data[8])
 
     json_worker = JsonWorker(cian_path)
-    # json_worker.write_json_flats(flats)
+    json_worker.write_json_flats(flats)
     json_worker.read_json_flats()
     info = json_worker.get_flats_info()
     new_list_flats = FlatsGenerator(info)
