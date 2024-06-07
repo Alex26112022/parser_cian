8a790302c7a8bf4489fd05e3696464f6ce30c55e
Алексей Денисенко <elvis.presly.039127@gmail.com>
2024-06-07 14:21:24 +0300

    fix parser

--- src/parser_cian_new.py
+++ src/parser_cian_new.py
@@ -15,116 +15,103 @@ class ParserCianNew:
 
     def __init__(self):
         self.info = []
-        self.total_cards = 1
-        self.total_pages = 1
         self.params = {'currency': '2', 'deal_type': 'sale',
                        'decorations_list%5B0%5D': 'without',
                        'engine_version': '2', 'house_material%5B0%5D': '2',
-                       'maxfloor': '10', 'maxprice': '20000000',
+                       'maxfloor': '10', 'maxprice': '11000000',
                        'maxtarea': '100', 'minfloor': '2',
                        'minprice': '5000000', 'mintarea': '60',
                        'object_type%5B0%5D': '2', 'offer_type': 'flat',
-                       'only_flat': '1', 'p': '1', 'region': '4593',
+                       'only_flat': '1', 'p': 0, 'region': '4593',
                        'room3': '1'}
 
-    def get_total_pages(self):
-        """ Определяет количество страниц для пагинации. """
+    def get_total_flats(self):
+        """ Определяет общее количество квартир в объявлениях. """
         response = requests.get(self.__url, headers=self.__headers,
                                 params=self.params)
 
         soup = BeautifulSoup(response.text, 'lxml')
-        total_cards_ = soup.find('h5').text.split()[1:-1]
-        if len(total_cards_) > 1:
-            total_cards_ = int(''.join(total_cards_))
-        else:
-            total_cards_ = int(total_cards_[0])
-        self.total_cards = total_cards_
-        print(total_cards_)
-        self.total_pages = int(self.total_cards) // 28
-        print(self.total_pages)
+        total_flats = soup.find('h5').text
+        return total_flats
 
     def get_response(self):
-        for page_ in range(1, self.total_pages + 1):
-            response = requests.get(self.__url, headers=self.__headers,
-                                    params=self.params)
+        while True:
+            try:
+                self.params['p'] += 1
+                print(f'Страница... {self.params['p']}')
+                response = requests.get(self.__url, headers=self.__headers,
+                                        params=self.params)
 
-            soup = BeautifulSoup(response.text, 'lxml')
+                soup = BeautifulSoup(response.text, 'lxml')
 
-            div_cards = soup.find_all('article',
-                                      class_='_93444fe79c--container--Povoi _93444fe79c--cont--OzgVc')
-            print(len(div_cards))
-            # FIXME
-            # for card in div_cards:
-            #     header_card = card.find('a', class_='_93444fe79c--link--VtWj6')
-            #     card_title = header_card.find('span').find('span').text.split()
-            #
-            #     room = int(card_title[0][0])
-            #     area = float(card_title[2].replace(',', '.'))
-            #     floor = card_title[-2]
-            #     card_url = header_card.get('href')
-            #
-            #     complex_ = card.find('a', class_='_93444fe79c--jk--dIktL').text
-            #
-            #     date_of_finish = card.find('span',
-            #                                class_='_93444fe79c--color_gray60_100--mYFjS '
-            #                                       '_93444fe79c--lineHeight_20px--fX7_V '
-            #                                       '_93444fe79c--fontWeight_normal--JEG_c '
-            #                                       '_93444fe79c--fontSize_14px--reQMB '
-            #                                       '_93444fe79c--display_inline--ySCqY '
-            #                                       '_93444fe79c--text--e4SBY '
-            #                                       '_93444fe79c--text_letterSpacing__normal--tfToq').text
-            #
-            #     address_ifo = card.find_all('a',
-            #                                 class_='_93444fe79c--link--NQlVc')
-            #     address = ''
-            #
-            #     for el in address_ifo:
-            #         address += f'{el.text}. '
-            #
-            #     price = card.find('span',
-            #                       class_='_93444fe79c--color_black_100--Ephi7 '
-            #                              '_93444fe79c--lineHeight_28px--KFXmc '
-            #                              '_93444fe79c--fontWeight_bold--BbhnX '
-            #                              '_93444fe79c--fontSize_22px--sFuaL '
-            #                              '_93444fe79c--display_block--KYb25 '
-            #                              '_93444fe79c--text--e4SBY '
-            #                              '_93444fe79c--text_letterSpacing__normal--tfToq')
-            #     price = int(price.find('span').text[:-1].replace(' ', ''))
-            #
-            #     description = card.find('p',
-            #                             class_='_93444fe79c--color_black_100--Ephi7 '
-            #                                    '_93444fe79c--lineHeight_20px--fX7_V '
-            #                                    '_93444fe79c--fontWeight_normal--JEG_c '
-            #                                    '_93444fe79c--fontSize_14px--reQMB '
-            #                                    '_93444fe79c--display_block--KYb25 '
-            #                                    '_93444fe79c--text--e4SBY '
-            #                                    '_93444fe79c--text_letterSpacing__normal--tfToq').text
-            #
-            #     type_of_developer = card.find('span',
-            #                                   class_='_93444fe79c--color_gray60_100--mYFjS '
-            #                                          '_93444fe79c--lineHeight_4u--E1SPG '
-            #                                          '_93444fe79c--fontWeight_bold--BbhnX '
-            #                                          '_93444fe79c--fontSize_10px--c1NGZ '
-            #                                          '_93444fe79c--display_block--KYb25 '
-            #                                          '_93444fe79c--text--e4SBY '
-            #                                          '_93444fe79c--text_textTransform__uppercase--JygPH').text
-            #
-            #     developer = card.find('span',
-            #                           class_='_93444fe79c--color_current_color--vhuGI '
-            #                                  '_93444fe79c--lineHeight_6u--cedXD '
-            #                                  '_93444fe79c--fontWeight_bold--BbhnX '
-            #                                  '_93444fe79c--fontSize_16px--QNYmt '
-            #                                  '_93444fe79c--display_block--KYb25 '
-            #                                  '_93444fe79c--text--e4SBY').text
-            #
-            #     full_data = (room, area, floor, price, address, complex_,
-            #                  date_of_finish, description, type_of_developer,
-            #                  developer, card_url)
-            #
-            #     self.info.append(full_data)
-            #
-            # self.params['p'] = str(page_+1)
-            # sleep(randint(1, 3))
+                div_cards = soup.find_all('article',
+                                          class_='_93444fe79c--container--Povoi _93444fe79c--cont--OzgVc')
+                print(len(div_cards))
+
+                for card in div_cards:
+                    header_card = card.find('a', class_='_93444fe79c--link--VtWj6')
+                    card_title = header_card.find('span').find('span').text.split()
+                    room = int(card_title[0][0])
+                    area = float(card_title[2].replace(',', '.'))
+                    floor = card_title[-2]
+                    card_url = header_card.get('href')
+
+                    complex_ = card.find('a', class_='_93444fe79c--jk--dIktL').text
+
+                    date_of_finish = card.find('span',
+                                               class_='_93444fe79c--color_gray60_100--mYFjS '
+                                                      '_93444fe79c--lineHeight_20px--fX7_V '
+                                                      '_93444fe79c--fontWeight_normal--JEG_c '
+                                                      '_93444fe79c--fontSize_14px--reQMB '
+                                                      '_93444fe79c--display_inline--ySCqY '
+                                                      '_93444fe79c--text--e4SBY '
+                                                      '_93444fe79c--text_letterSpacing__normal--tfToq').text
+
+                    address_ifo = card.find_all('a',
+                                                class_='_93444fe79c--link--NQlVc')
+                    address = ''
+
+                    for el in address_ifo:
+                        address += f'{el.text}. '
+
+                    price = card.find('span', {'data-mark': 'MainPrice'})
+                    price = int(price.find('span').text[:-1].replace(' ', ''))
+
+                    description = card.find('p',
+                                            class_='_93444fe79c--color_black_100--Ephi7 '
+                                                   '_93444fe79c--lineHeight_20px--fX7_V '
+                                                   '_93444fe79c--fontWeight_normal--JEG_c '
+                                                   '_93444fe79c--fontSize_14px--reQMB '
+                                                   '_93444fe79c--display_block--KYb25 '
+                                                   '_93444fe79c--text--e4SBY '
+                                                   '_93444fe79c--text_letterSpacing__normal--tfToq').text
+
+                    type_of_developer = card.find('span',
+                                                  class_='_93444fe79c--color_gray60_100--mYFjS '
+                                                         '_93444fe79c--lineHeight_4u--E1SPG '
+                                                         '_93444fe79c--fontWeight_bold--BbhnX '
+                                                         '_93444fe79c--fontSize_10px--c1NGZ '
+                                                         '_93444fe79c--display_block--KYb25 '
+                                                         '_93444fe79c--text--e4SBY '
+                                                         '_93444fe79c--text_textTransform__uppercase--JygPH').text
+
+                    developer = card.find('span',
+                                          class_='_93444fe79c--color_current_color--vhuGI '
+                                                 '_93444fe79c--lineHeight_6u--cedXD '
+                                                 '_93444fe79c--fontWeight_bold--BbhnX '
+                                                 '_93444fe79c--fontSize_16px--QNYmt '
+                                                 '_93444fe79c--display_block--KYb25 '
+                                                 '_93444fe79c--text--e4SBY').text
+
+                    full_data = (room, area, floor, price, address, complex_,
+                                 date_of_finish, description, type_of_developer,
+                                 developer, card_url)
+
+                    self.info.append(full_data)
+
+                sleep(randint(1, 3))
+            except:
+                return
 
     def get_info_cards(self):
         """ Возвращает список кортежей данных по объявлениям. """
@@ -132,8 +119,8 @@ class ParserCianNew:
 
 
 new_pars = ParserCianNew()
-# new_pars.get_total_pages()
+print(new_pars.get_total_flats())
 
-new_pars.get_response()
+# new_pars.get_response()
 # print(len(new_pars.get_info_cards()))
 # print(new_pars.get_info_cards())
